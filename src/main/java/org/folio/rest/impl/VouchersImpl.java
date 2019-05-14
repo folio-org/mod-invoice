07b87c99789d6d1db0fd326cd65ad32db1f48283
javalkar-varun <vjavalkar@ebsco.com>
2019-05-14 16:08:11 -0400

    Fix voucher.raml

--- src/main/java/org/folio/rest/impl/VouchersImpl.java
+++ src/main/java/org/folio/rest/impl/VouchersImpl.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 import javax.ws.rs.core.Response;
 import org.folio.rest.annotations.Validate;
-import org.folio.rest.jaxrs.model.VoucherLine;
+
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
@@ -24,44 +24,15 @@ public class VouchersImpl implements org.folio.rest.jaxrs.resource.Voucher {
     return null;
   }
 
-  @Override
-  public void postVoucherVoucherLines(String lang, VoucherLine entity, Map<String, String> okapiHeaders,
-      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    // TODO Auto-generated method stub
-    
-  }
-
   @Validate
   @Override
   public void getVoucherVoucherLinesById(String id, String lang, Map<String, String> okapiHeaders,
-      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+                                         Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     logger.info("== Get Voucher Line by Id for an existing invoice ==");
     VoucherLineHelper voucherLineHelper = new VoucherLineHelper(okapiHeaders, vertxContext, lang);
     voucherLineHelper
       .getVoucherLines(id)
       .thenAccept(voucherLine -> asyncResultHandler.handle(succeededFuture(voucherLineHelper.buildOkResponse(voucherLine))))
       .exceptionally(t -> handleErrorResponse(asyncResultHandler, voucherLineHelper, t));
-    
-  }
-
-  @Override
-  public void deleteVoucherVoucherLinesById(String id, String lang, Map<String, String> okapiHeaders,
-      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void putVoucherVoucherLinesById(String id, String lang, VoucherLine entity, Map<String, String> okapiHeaders,
-      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void getVoucherVoucherLines(String lang, Map<String, String> okapiHeaders,
-      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    // TODO Auto-generated method stub
-    
   }
 }
