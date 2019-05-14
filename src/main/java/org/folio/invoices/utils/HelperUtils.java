8674bcd49376822950bab83233a9f1e710b310c6
javalkar-varun <vjavalkar@ebsco.com>
2019-05-14 16:15:58 -0400

    Remove redundant code

--- src/main/java/org/folio/invoices/utils/HelperUtils.java
+++ src/main/java/org/folio/invoices/utils/HelperUtils.java
@@ -30,7 +30,6 @@ public class HelperUtils {
 
   public static final String URL_WITH_LANG_PARAM = "%s?lang=%s";
   private static final String GET_INVOICE_BYID = resourceByIdPath(INVOICES) + URL_WITH_LANG_PARAM;
-  private static final String GET_INVOICE_LINES_BYID = resourceByIdPath(INVOICE_LINES) + URL_WITH_LANG_PARAM;
   private static final String GET_VOUCHER_LINES_BYID = resourceByIdPath(VOUCHER_LINES) + URL_WITH_LANG_PARAM;
 
   private HelperUtils() {
@@ -44,12 +43,6 @@ public class HelperUtils {
   public static CompletableFuture<JsonObject> getInvoiceById(String id, String lang, HttpClientInterface httpClient, Context ctx,
       Map<String, String> okapiHeaders, Logger logger) {
     String endpoint = String.format(GET_INVOICE_BYID, id, lang);
-    return handleGetRequest(endpoint, httpClient, ctx, okapiHeaders, logger);
-  }
-
-  public static CompletableFuture<JsonObject> getInvoiceLineById(String id, String lang, HttpClientInterface httpClient, Context ctx,
-                                                                 Map<String, String> okapiHeaders, Logger logger) {
-    String endpoint = String.format(GET_INVOICE_LINES_BYID, id, lang);
     return handleGetRequest(endpoint, httpClient, ctx, okapiHeaders, logger);
   }
 
