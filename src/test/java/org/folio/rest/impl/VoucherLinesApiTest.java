4e399a891571ae49028985e8459733de69757105
javalkar-varun <vjavalkar@ebsco.com>
2019-05-15 15:51:05 -0400

    Minor improvement - use String.format

--- src/test/java/org/folio/rest/impl/VoucherLinesApiTest.java
+++ src/test/java/org/folio/rest/impl/VoucherLinesApiTest.java
@@ -15,6 +15,8 @@ import java.net.MalformedURLException;
 
 import static org.folio.rest.impl.AbstractHelper.ID;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 
 public class VoucherLinesApiTest extends ApiTestBase {
@@ -23,7 +25,7 @@ public class VoucherLinesApiTest extends ApiTestBase {
 
   static final String VOUCHER_LINES_MOCK_DATA_PATH = BASE_MOCK_DATA_PATH + "voucherLines/";
   private static final String VOUCHER_LINES_LIST_PATH = VOUCHER_LINES_MOCK_DATA_PATH + "voucher_lines.json";
-  private static final String VOUCHER_LINES_PATH = "/voucher/voucher-lines";
+  private static final String VOUCHER_LINES_PATH = "/voucher/voucher-lines" + "/%s";
   private static final String NOT_FOUND_VOUCHER_LINE_ID = "5a34ae0e-5a11-4337-be95-1a20cfdc3161";
   private static final String INVALID_VOUCHER_LINE_ID = "invalidVoucherLineId";
   
@@ -35,7 +37,7 @@ public class VoucherLinesApiTest extends ApiTestBase {
     String id = voucherLinesList.getJsonArray("voucherLines").getJsonObject(0).getString(ID);
     logger.info(String.format("using mock datafile: %s%s.json", VOUCHER_LINES_LIST_PATH, id));
 
-    final VoucherLine resp = verifySuccessGet(VOUCHER_LINES_PATH + "/" + id, VoucherLine.class);
+    final VoucherLine resp = verifySuccessGet(String.format(VOUCHER_LINES_PATH, id), VoucherLine.class);
 
     logger.info(JsonObject.mapFrom(resp).encodePrettily());
     assertEquals(id, resp.getId());
@@ -45,7 +47,7 @@ public class VoucherLinesApiTest extends ApiTestBase {
   public void testGetVouchersVoucherLinesByIdNotFoundTest() throws MalformedURLException {
     logger.info("=== Test Get Voucher line by Id - 404 Not found ===");
 
-    final Response resp = verifyGet(VOUCHER_LINES_PATH + "/" + NOT_FOUND_VOUCHER_LINE_ID, APPLICATION_JSON, 404);
+    final Response resp = verifyGet(String.format(VOUCHER_LINES_PATH, NOT_FOUND_VOUCHER_LINE_ID), APPLICATION_JSON, 404);
 
     String actual = resp.getBody().as(Errors.class).getErrors().get(0).getMessage();
     logger.info("Id not found: " + actual);
@@ -56,6 +58,11 @@ public class VoucherLinesApiTest extends ApiTestBase {
   @Test
   public void testGetVouchersVoucherLineByIdInvalidFormat() {
     logger.info("=== Test Get Voucher line by Id - 400 Bad request ===");
-    verifyGet(String.format(VOUCHER_LINES_PATH, INVALID_VOUCHER_LINE_ID), TEXT_PLAIN, 400);
+    final Response resp = verifyGet(String.format(VOUCHER_LINES_PATH, INVALID_VOUCHER_LINE_ID), TEXT_PLAIN, 400);
+    String actual = resp.getBody().asString();
+    logger.info(actual);
+
+    assertNotNull(actual);
+    assertTrue(actual.contains(INVALID_VOUCHER_LINE_ID));
   }
 }
