3cf2be9df50d60ae1fa26a30ece4aebd61ac6307
Varun Javalkar <vjavalkar@ebsco.com>
2019-06-03 09:13:43 -0400

    MODINVOICE-57: Implement GET /voucher/vouchers (#28)
    * MODINVOICE-57 GET all vouchers

--- src/test/java/org/folio/rest/impl/MockServer.java
+++ src/test/java/org/folio/rest/impl/MockServer.java
@@ -27,6 +27,7 @@ import static org.folio.rest.impl.InvoicesApiTest.BAD_QUERY;
 import static org.folio.rest.impl.InvoicesApiTest.EXISTING_VENDOR_INV_NO;
 import static org.folio.rest.impl.InvoicesApiTest.INVOICE_MOCK_DATA_PATH;
 import static org.folio.rest.impl.VouchersApiTest.VOUCHER_MOCK_DATA_PATH;
+import static org.folio.rest.impl.VouchersApiTest.EXISTING_VOUCHER_NUMBER;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
@@ -132,6 +133,7 @@ public class MockServer {
     router.route(HttpMethod.GET, resourceByIdPath(VOUCHER_LINES, ID_PATH_PARAM)).handler(this::handleGetVoucherLineById);
     router.route(HttpMethod.GET, resourceByIdPath(VOUCHERS, ID_PATH_PARAM)).handler(this::handleGetVoucherById);
     router.route(HttpMethod.GET, resourcesPath(VOUCHER_NUMBER_START)).handler(this::handleGetSequence);
+    router.route(HttpMethod.GET, resourcesPath(VOUCHERS)).handler(this::handleGetVouchers);
 
     router.route(HttpMethod.DELETE, resourceByIdPath(INVOICES, ID_PATH_PARAM)).handler(ctx -> handleDeleteRequest(ctx, INVOICES));
     router.route(HttpMethod.DELETE, resourceByIdPath(INVOICE_LINES, ID_PATH_PARAM)).handler(ctx -> handleDeleteRequest(ctx, INVOICE_LINES));
@@ -411,7 +413,30 @@ public class MockServer {
     }
   }
 
-
+  private void handleGetVouchers(RoutingContext ctx) {
+    String queryParam = StringUtils.trimToEmpty(ctx.request().getParam("query"));
+    if (queryParam.contains(BAD_QUERY)) {
+      serverResponse(ctx, 400, APPLICATION_JSON, Response.Status.BAD_REQUEST.getReasonPhrase());
+    } else if (queryParam.contains(ID_FOR_INTERNAL_SERVER_ERROR)) {
+      serverResponse(ctx, 500, APPLICATION_JSON, Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase());
+    } else {
+      JsonObject voucher = new JsonObject();
+      final String VOUCHER_NUMBER_QUERY = "voucherNumber==";
+      switch (queryParam) {
+        case VOUCHER_NUMBER_QUERY + EXISTING_VOUCHER_NUMBER:
+          voucher.put(TOTAL_RECORDS, 1);
+          break;
+        case EMPTY:
+          voucher.put(TOTAL_RECORDS, 4);
+          break;
+        default:
+          voucher.put(TOTAL_RECORDS, 0);
+      }
+      addServerRqRsData(HttpMethod.GET, VOUCHERS, voucher);
+      serverResponse(ctx, 200, APPLICATION_JSON, voucher.encodePrettily());
+    }
+  }
+  
   private void handleGetSequence(RoutingContext ctx) {
     if (ERROR_TENANT.equals(ctx.request().getHeader(OKAPI_HEADER_TENANT))) {
       serverResponse(ctx, 500, TEXT_PLAIN, INTERNAL_SERVER_ERROR.getReasonPhrase());
