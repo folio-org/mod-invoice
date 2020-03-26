1ca896afca3b6c26c7687578ebec95f0bf96170a
Yauheni Kuzmianok <yauheni_kuzmianok@epam.com>
2020-03-26 14:48:14 +0300

    Modinvoice 100 (#115)
    * [MODINVOICE-100] Fix raml(contract) to return application/json responses wherever possible

--- src/main/java/org/folio/rest/impl/BatchVoucherExportsImpl.java
+++ src/main/java/org/folio/rest/impl/BatchVoucherExportsImpl.java
@@ -94,13 +94,13 @@ public class BatchVoucherExportsImpl implements BatchVoucherBatchVoucherExports 
   @Override
   public void postBatchVoucherBatchVoucherExportsUploadById(String id, String lang, Map<String, String> okapiHeaders,
       Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(PostBatchVoucherBatchVoucherExportsUploadByIdResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(PostBatchVoucherBatchVoucherExportsUploadByIdResponse.respond500WithApplicationJson(NOT_SUPPORTED)));
   }
 
   @Validate
   @Override
   public void postBatchVoucherBatchVoucherExportsScheduled(String lang, Map<String, String> okapiHeaders,
       Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(PostBatchVoucherBatchVoucherExportsScheduledResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(PostBatchVoucherBatchVoucherExportsScheduledResponse.respond500WithApplicationJson(NOT_SUPPORTED)));
   }
 }
