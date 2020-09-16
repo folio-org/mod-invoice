825d5c485bf11c709da362a2830db29c3bd926ad
Andrei_Makaranka <Andrei_makaranka@epam.com>
2020-09-16 13:53:43 +0300

    [MODINVOICE-193] - Initial

--- src/main/java/org/folio/services/ftp/FtpUploadService.java
+++ src/main/java/org/folio/services/ftp/FtpUploadService.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 
@@ -13,10 +14,11 @@ import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.folio.exceptions.FtpException;
 
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
+import io.vertx.core.Handler;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import me.escoffier.vertx.completablefuture.VertxCompletableFuture;
 
 public class FtpUploadService implements UploadService {
 
@@ -26,15 +28,22 @@ public class FtpUploadService implements UploadService {
   private final FTPClient ftp;
   private final String server;
   private final int port;
+  private final Context ctx;
 
-  public FtpUploadService(String uri) throws URISyntaxException {
+  public FtpUploadService(Context ctx, String uri) throws URISyntaxException {
     if (!isUriValid(uri)) {
       throw new URISyntaxException(uri, "URI should be valid ftp path");
     }
     this.ftp = new FTPClient();
+    this.ftp.setDefaultTimeout(30000);
+    this.ftp.addProtocolCommandListener(FTPVertxCommandLogger.getDefListener(logger));
+    this.ftp.setControlKeepAliveTimeout(30);
+    this.ftp.setBufferSize(1024 * 1024);
+    this.ftp.setPassiveNatWorkaroundStrategy(new DefaultServerResolver(ftp));
     URI u = new URI(uri);
     this.server = u.getHost();
     this.port = u.getPort() > 0 ? u.getPort() : 21;
+    this.ctx = ctx;
   }
 
   public boolean isUriValid(String uri) throws URISyntaxException {
@@ -43,72 +52,95 @@ public class FtpUploadService implements UploadService {
   }
 
   public CompletableFuture<String> login(String username, String password) {
-    return CompletableFuture.supplyAsync(() -> {
+    CompletableFuture<String> future = new CompletableFuture<>();
+    ctx.owner().executeBlocking(blockingFeature -> {
       try {
         ftp.connect(server, port);
-        if (logger.isInfoEnabled()) {
-          logger.info("Connected to {}:{}", server, port);
+        logger.info("Connected to {}:{}", server, port);
+        if (ftp.login(username, password)) {
+          blockingFeature.complete(ftp.getReplyString().trim());
+        } else {
+          blockingFeature.fail(new FtpException(ftp.getReplyCode(), ftp.getReplyString().trim()));
         }
       } catch (Exception e) {
         logger.error("Error Connecting", e);
-        throw new CompletionException(e);
+        disconnect();
+        blockingFeature.fail(new CompletionException(e));
       }
-
-      try {
-        if (ftp.login(username, password)) {
-          return ftp.getReplyString().trim();
-        } else {
-          throw new FtpException(ftp.getReplyCode(), ftp.getReplyString().trim());
-        }
-      } catch (Exception e) {
-        logger.error("Error logging in", e);
-        throw new CompletionException(e);
-      }
-    });
+        blockingFeature.complete();
+      },
+      false,
+      asyncResultHandler(future, "Success login to FTP", "Failed login to FTP"));
+    return future;
   }
 
   public CompletableFuture<String> logout() {
-    return CompletableFuture.supplyAsync(() -> {
-      try {
-        if (ftp != null && ftp.isConnected()) {
-          ftp.logout();
-          return ftp.getReplyString().trim();
+    CompletableFuture<String> future = new CompletableFuture<>();
+    ctx.owner().executeBlocking(blockingFeature -> {
+        try {
+          if (ftp != null && ftp.isConnected()) {
+            ftp.logout();
+            blockingFeature.complete(ftp.getReplyString().trim());
+          }
+          blockingFeature.complete(null);
+        } catch (Exception e) {
+          logger.error("Error logging out", e);
+          blockingFeature.fail(new CompletionException(e));
+        } finally {
+          disconnect();
         }
-        return null;
-      } catch (Exception e) {
-        logger.error("Error logging out", e);
-        throw new CompletionException(e);
+      },
+        false,
+        asyncResultHandler(future, "Success logout from FTP", "Failed logout from FTP")
+      );
+      return future;
+  }
+
+  private Handler<AsyncResult<Object>> asyncResultHandler(CompletableFuture<String> future, String s, String s2) {
+    return result -> {
+      if (result.succeeded()) {
+        logger.debug(s);
+        future.complete(result.result().toString());
+      } else {
+        String message = Optional.ofNullable(result.cause())
+          .map(Throwable::getMessage)
+          .orElse(s2);
+        logger.error(message);
+        future.completeExceptionally(result.cause());
       }
-    });
+    };
   }
 
   public CompletableFuture<String> upload(Context ctx, String filename, String content) {
-  return VertxCompletableFuture.supplyBlockingAsync(ctx, () -> {
+    CompletableFuture<String> future = new CompletableFuture<>();
+    ctx.owner().executeBlocking(blockingFeature -> {
       try (InputStream is = new ByteArrayInputStream(content.getBytes())) {
-        ftp.addProtocolCommandListener( FTPVertxCommandLogger.getDefListener(logger));
-        ftp.setBufferSize(1024 * 1024);
-        ftp.setConnectTimeout(30000);
         ftp.setFileType(FTP.BINARY_FILE_TYPE);
         ftp.setPassiveNatWorkaroundStrategy(new DefaultServerResolver(ftp));
         ftp.enterLocalPassiveMode();
         changeWorkingDirectory();
         if (ftp.storeFile(filename, is)) {
-          return ftp.getReplyString().trim();
+          logger.debug("Batch voucher uploaded on FTP");
+          blockingFeature.complete(ftp.getReplyString().trim());
         } else {
-          throw new FtpException(ftp.getReplyCode(), ftp.getReplyString().trim());
+          blockingFeature.fail(new FtpException(ftp.getReplyCode(), ftp.getReplyString().trim()));
         }
       } catch (Exception e) {
         logger.error("Error uploading", e);
-        throw new CompletionException(e);
+        blockingFeature.fail(new CompletionException(e));
       } finally {
         try {
           ftp.logout();
-          ftp.disconnect();
         } catch (IOException e) {
-          logger.error("Error logout", e);
+          logger.error("Error logout from FTP", e);
+        } finally {
+          disconnect();
         }
       }
-    });
+    },
+      false,
+      asyncResultHandler(future, "Success upload to FTP", "Failed upload to FTP"));
+    return future;
   }
 
   private void changeWorkingDirectory() throws IOException {
@@ -134,6 +166,14 @@ public class FtpUploadService implements UploadService {
     @Override
     public String resolve(String hostname) {
       return this.client.getRemoteAddress().getHostAddress();
+    }
+  }
+
+  private void disconnect() {
+    try {
+      ftp.disconnect();
+    } catch (IOException e) {
+      logger.error("Error disconnect from FTP", e);
     }
   }
 
