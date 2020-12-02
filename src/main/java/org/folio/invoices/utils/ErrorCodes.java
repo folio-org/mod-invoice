06d993def88dfffff6cdca23fe6500a4656edbc9
Andrei_Makaranka <Andrei_makaranka@epam.com>
2020-12-02 12:17:10 +0300

    [MODINVOICE-214] - Delete unused error code

--- src/main/java/org/folio/invoices/utils/ErrorCodes.java
+++ src/main/java/org/folio/invoices/utils/ErrorCodes.java
@@ -20,7 +20,6 @@ public enum ErrorCodes {
   LINE_FUND_DISTRIBUTIONS_SUMMARY_MISMATCH("lineFundDistributionsSummaryMismatch", "Fund distributions summary should be 100 % or equal to subtotal for every associated invoice lines"),
   ADJUSTMENT_FUND_DISTRIBUTIONS_SUMMARY_MISMATCH("adjustmentFundDistributionsSummaryMismatch", "Fund distributions summary should be 100 % or equal to subtotal for every non-prorated adjustment"),
   VOUCHER_UPDATE_FAILURE("voucherUpdateFailure", "Voucher record cannot be updated"),
-  INVOICE_TOTAL_REQUIRED("invoiceTotalRequired", "The total amount is expected when lockTotal is true"),
   MOD_CONFIG_ERROR("configNotAvailable", "The mod-configuration is not available"),
   PROHIBITED_FIELD_CHANGING("protectedFieldChanging", "Field can't be modified"),
   FUNDS_NOT_FOUND("fundsNotFound", "Fund records are not found"),
