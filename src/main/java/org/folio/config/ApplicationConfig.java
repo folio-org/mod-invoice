05900ce06b5fcf9f3b0753fa325c1b6e3a10b9e1
Ruslan Lavrov <ruslan_lavrov@epam.com>
2021-02-26 15:59:14 +0200

    Fixed smells

--- src/main/java/org/folio/config/ApplicationConfig.java
+++ src/main/java/org/folio/config/ApplicationConfig.java
@@ -1,8 +1,5 @@
 package org.folio.config;
 
-import org.folio.kafka.KafkaConfig;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
