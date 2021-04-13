22a380652655272736bebd9ecf1a6c9d09f9ec47
damien-git <damien-git@users.noreply.github.com>
2021-04-13 03:03:04 -0400

    [MODINVOICE-240] Added vendor address to XML schema for batched vouchers and updated converter and tests (#236)

--- src/test/java/org/folio/converters/BatchedVoucherModelConverterTest.java
+++ src/test/java/org/folio/converters/BatchedVoucherModelConverterTest.java
@@ -1,8 +1,9 @@
 package org.folio.converters;
 
 import static org.folio.jaxb.JAXBUtil.convertOldJavaDate;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -12,8 +13,10 @@ import java.nio.file.Paths;
 
 import org.folio.rest.jaxrs.model.BatchVoucher;
 import org.folio.rest.jaxrs.model.BatchedVoucher;
+import org.folio.rest.jaxrs.model.VendorAddress;
 import org.folio.rest.jaxrs.model.jaxb.BatchedVoucherType;
 import org.folio.rest.jaxrs.model.jaxb.PaymentAccountType;
+import org.folio.rest.jaxrs.model.jaxb.VendorAddressType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -26,7 +29,7 @@ import io.vertx.core.json.JsonObject;
 public class BatchedVoucherModelConverterTest {
   public static final String RESOURCES_PATH = "src/test/resources/mockdata/batchVouchers";
   public static final String VALID_BATCH_VOUCHER_JSON = "35657479-83b9-4760-9c39-b58dcd02ee14.json";
-  private static Path JSON_BATCH_VOUCHER_PATH = Paths.get(RESOURCES_PATH, VALID_BATCH_VOUCHER_JSON).toAbsolutePath();
+  private static final Path JSON_BATCH_VOUCHER_PATH = Paths.get(RESOURCES_PATH, VALID_BATCH_VOUCHER_JSON).toAbsolutePath();
 
   @InjectMocks
   BatchedVoucherModelConverter converter;
@@ -39,14 +42,32 @@ public class BatchedVoucherModelConverterTest {
     BatchVoucher json = new JsonObject(contents).mapTo(BatchVoucher.class);
     BatchedVoucher batchedVoucher = json.getBatchedVouchers().get(0);
     BatchedVoucherType batchedVoucherType = converter.convert(batchedVoucher);
+    assertNotNull(batchedVoucherType);
+    checkEquals(batchedVoucherType, batchedVoucher);
+  }
 
+  @Test
+  public void testShouldSuccessConvertJSONModelToXMLModelIfDatesAreNull() throws IOException {
+    String contents = new String(Files.readAllBytes(JSON_BATCH_VOUCHER_PATH));
+    BatchVoucher json = new JsonObject(contents).mapTo(BatchVoucher.class);
+    BatchedVoucher batchedVoucher = json.getBatchedVouchers().get(0);
+    batchedVoucher.setDisbursementDate(null);
+    BatchedVoucherType batchedVoucherType = converter.convert(batchedVoucher);
+    assertNotNull(batchedVoucherType);
+    checkEquals(batchedVoucherType, batchedVoucher);
+  }
+
+  private void checkEquals(BatchedVoucherType batchedVoucherType, BatchedVoucher batchedVoucher) {
     assertEquals(batchedVoucher.getVoucherNumber(), batchedVoucherType.getVoucherNumber());
     assertEquals(batchedVoucher.getAccountingCode(), batchedVoucherType.getAccountingCode());
     assertEquals(batchedVoucher.getAccountNo(), batchedVoucherType.getAccountNo());
     assertEquals(BigDecimal.valueOf(batchedVoucher.getAmount()), batchedVoucherType.getAmount());
 
     assertEquals(batchedVoucher.getDisbursementNumber(), batchedVoucherType.getDisbursementNumber());
-    assertEquals(convertOldJavaDate(batchedVoucher.getDisbursementDate()), batchedVoucherType.getDisbursementDate());
+    if (batchedVoucher.getDisbursementDate() == null)
+      assertNull(batchedVoucherType.getDisbursementDate());
+    else
+      assertEquals(convertOldJavaDate(batchedVoucher.getDisbursementDate()), batchedVoucherType.getDisbursementDate());
     assertEquals(BigDecimal.valueOf(batchedVoucher.getAmount()), batchedVoucherType.getDisbursementAmount());
 
     assertEquals(BigDecimal.valueOf(batchedVoucher.getExchangeRate()), batchedVoucherType.getExchangeRate());
@@ -60,36 +81,19 @@ public class BatchedVoucherModelConverterTest {
     assertEquals(batchedVoucher.getInvoiceNote(), batchedVoucherType.getInvoiceNote());
     assertEquals(batchedVoucher.getStatus().toString(), batchedVoucherType.getStatus());
     assertEquals(batchedVoucher.getEnclosureNeeded(), batchedVoucherType.isEnclosureNeeded());
-    assertEquals(batchedVoucher.getBatchedVoucherLines().size(), batchedVoucherType.getBatchedVoucherLines().getBatchedVoucherLine().size());
-  }
 
-  @Test
-  public void testShouldSuccessConvertJSONModelToXMLModelIfDatesAreNull() throws IOException {
-    String contents = new String(Files.readAllBytes(JSON_BATCH_VOUCHER_PATH));
-    BatchVoucher json = new JsonObject(contents).mapTo(BatchVoucher.class);
-    BatchedVoucher batchedVoucher = json.getBatchedVouchers().get(0);
-    batchedVoucher.setDisbursementDate(null);
-    BatchedVoucherType batchedVoucherType = converter.convert(batchedVoucher);
+    if (batchedVoucher.getVendorAddress() != null) {
+      VendorAddress address = batchedVoucher.getVendorAddress();
+      VendorAddressType convertedAddress = batchedVoucherType.getVendorAddress();
+      assertEquals(address.getAddressLine1(), convertedAddress.getAddressLine1());
+      assertEquals(address.getAddressLine2(), convertedAddress.getAddressLine2());
+      assertEquals(address.getCity(), convertedAddress.getCity());
+      assertEquals(address.getStateRegion(), convertedAddress.getStateRegion());
+      assertEquals(address.getZipCode(), convertedAddress.getZipCode());
+      assertEquals(address.getCountry(), convertedAddress.getCountry());
+    }
 
-    assertEquals(batchedVoucher.getVoucherNumber(), batchedVoucherType.getVoucherNumber());
-    assertEquals(batchedVoucher.getAccountingCode(), batchedVoucherType.getAccountingCode());
-    assertEquals(BigDecimal.valueOf(batchedVoucher.getAmount()), batchedVoucherType.getAmount());
-
-    assertEquals(batchedVoucher.getDisbursementNumber(), batchedVoucherType.getDisbursementNumber());
-    assertNull(batchedVoucherType.getDisbursementDate());
-    assertEquals(BigDecimal.valueOf(batchedVoucher.getAmount()), batchedVoucherType.getDisbursementAmount());
-
-    assertEquals(BigDecimal.valueOf(batchedVoucher.getExchangeRate()), batchedVoucherType.getExchangeRate());
-    assertEquals(batchedVoucher.getInvoiceCurrency(), batchedVoucherType.getInvoiceCurrency());
-    assertEquals(batchedVoucher.getSystemCurrency(), batchedVoucherType.getSystemCurrency());
-    assertEquals(PaymentAccountType.fromValue(batchedVoucher.getType()
-      .toString()), batchedVoucherType.getType());
-
-    assertEquals(batchedVoucher.getVendorInvoiceNo(), batchedVoucherType.getVendorInvoiceNo());
-    assertEquals(batchedVoucher.getVendorName(), batchedVoucherType.getVendorName());
-    assertEquals(convertOldJavaDate(batchedVoucher.getVoucherDate()), batchedVoucherType.getVoucherDate());
-    assertEquals(batchedVoucher.getInvoiceNote(), batchedVoucherType.getInvoiceNote());
-    assertEquals(batchedVoucher.getStatus().toString(), batchedVoucherType.getStatus());
-    assertEquals(batchedVoucher.getBatchedVoucherLines().size(), batchedVoucherType.getBatchedVoucherLines().getBatchedVoucherLine().size());
+    assertEquals(batchedVoucher.getBatchedVoucherLines().size(),
+      batchedVoucherType.getBatchedVoucherLines().getBatchedVoucherLine().size());
   }
 }
