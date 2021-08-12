e15463aaadcabed5a7f81bbcb135feefa3ecc3c8
Oleksandr_Oliinyk2 <oleksandr_oliinyk2@epam.com>
2021-08-12 09:57:48 +0300

    Invoice poNumbers needs to be updated when an invoice line is deleted

---
+++ src/main/java/org/folio/rest/util/InvoiceHolder.java
@@ -0,0 +1,28 @@
+package org.folio.rest.util;
+
+import org.folio.rest.jaxrs.model.Invoice;
+import org.folio.rest.jaxrs.model.InvoiceLine;
+
+public class InvoiceHolder {
+
+  private InvoiceLine invoiceLine;
+  private Invoice invoice;
+
+  public InvoiceLine getInvoiceLine() {
+    return invoiceLine;
+  }
+
+  public InvoiceHolder setInvoiceLine(InvoiceLine invoiceLine) {
+    this.invoiceLine = invoiceLine;
+    return this;
+  }
+
+  public Invoice getInvoice() {
+    return invoice;
+  }
+
+  public InvoiceHolder setInvoice(Invoice invoice) {
+    this.invoice = invoice;
+    return this;
+  }
+}
