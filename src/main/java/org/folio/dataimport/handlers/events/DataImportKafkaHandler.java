78a5ff9389f23a56f54b295eda9f8883bf76bc11
Ruslan Lavrov <ruslan_lavrov@epam.com>
2021-09-10 20:59:07 +0300

    Added jobProfileSnapshot cache

--- src/main/java/org/folio/dataimport/handlers/events/DataImportKafkaHandler.java
+++ src/main/java/org/folio/dataimport/handlers/events/DataImportKafkaHandler.java
@@ -1,37 +1,49 @@
 package org.folio.dataimport.handlers.events;
 
-import static org.folio.DataImportEventTypes.DI_ERROR;
-
-import java.io.IOException;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.folio.DataImportEventPayload;
-import org.folio.dataimport.InvoiceWriterFactory;
-import org.folio.dataimport.handlers.actions.CreateInvoiceEventHandler;
-import org.folio.kafka.AsyncRecordHandler;
-import org.folio.processing.events.EventManager;
-import org.folio.processing.events.utils.ZIPArchiver;
-import org.folio.processing.mapping.MappingManager;
-import org.folio.processing.mapping.mapper.reader.record.edifact.EdifactReaderFactory;
-import org.folio.rest.core.RestClient;
-import org.folio.rest.jaxrs.model.Event;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.jackson.DatabindCodec;
 import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.folio.DataImportEventPayload;
+import org.folio.dataimport.InvoiceWriterFactory;
+import org.folio.dataimport.cache.JobProfileSnapshotCache;
+import org.folio.dataimport.handlers.actions.CreateInvoiceEventHandler;
+import org.folio.kafka.AsyncRecordHandler;
+import org.folio.processing.events.EventManager;
+import org.folio.processing.events.utils.ZIPArchiver;
+import org.folio.processing.exceptions.EventProcessingException;
+import org.folio.processing.mapping.MappingManager;
+import org.folio.processing.mapping.mapper.reader.record.edifact.EdifactReaderFactory;
+import org.folio.rest.RestConstants;
+import org.folio.rest.RestVerticle;
+import org.folio.rest.core.RestClient;
+import org.folio.rest.jaxrs.model.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+import static java.lang.String.format;
+import static org.folio.DataImportEventTypes.DI_ERROR;
 
 @Component
 public class DataImportKafkaHandler implements AsyncRecordHandler<String, String> {
 
+  public static final String JOB_PROFILE_SNAPSHOT_ID_KEY = "JOB_PROFILE_SNAPSHOT_ID";
+  private static final String PROFILE_SNAPSHOT_NOT_FOUND_MSG = "JobProfileSnapshot was not found by id '%s'";
+
   private final Logger logger = LogManager.getLogger(DataImportKafkaHandler.class);
 
+  private final JobProfileSnapshotCache profileSnapshotCache;
+
   @Autowired
-  public DataImportKafkaHandler(RestClient restClient) {
+  public DataImportKafkaHandler(RestClient restClient, JobProfileSnapshotCache profileSnapshotCache) {
+    this.profileSnapshotCache = profileSnapshotCache;
     MappingManager.registerReaderFactory(new EdifactReaderFactory());
     MappingManager.registerWriterFactory(new InvoiceWriterFactory());
     EventManager.registerEventHandler(new CreateInvoiceEventHandler(restClient));
@@ -45,19 +57,32 @@ public class DataImportKafkaHandler implements AsyncRecordHandler<String, String
       DataImportEventPayload eventPayload = new JsonObject(ZIPArchiver.unzip(event.getEventPayload())).mapTo(DataImportEventPayload.class);
       logger.info("Data import event payload has been received with event type: {}", eventPayload.getEventType());
 
-      EventManager.handleEvent(eventPayload).whenComplete((processedPayload, throwable) -> {
-        if (throwable != null) {
-          promise.fail(throwable);
-        } else if (DI_ERROR.value().equals(processedPayload.getEventType())) {
-          promise.fail("Failed to process data import event payload");
-        } else {
-          promise.complete(kafkaRecord.key());
-        }
-      });
+      String profileSnapshotId = eventPayload.getContext().get(JOB_PROFILE_SNAPSHOT_ID_KEY);
+      Map<String, String> okapiHeaders = retrieveOkapiHeaders(eventPayload);
+
+      profileSnapshotCache.get(profileSnapshotId, okapiHeaders)
+        .thenCompose(snapshotOptional -> snapshotOptional
+          .map(profileSnapshot -> EventManager.handleEvent(eventPayload, profileSnapshot))
+          .orElse(CompletableFuture.failedFuture(new EventProcessingException(format(PROFILE_SNAPSHOT_NOT_FOUND_MSG, profileSnapshotId)))))
+        .whenComplete((processedPayload, throwable) -> {
+          if (throwable != null) {
+            promise.fail(throwable);
+          } else if (DI_ERROR.value().equals(processedPayload.getEventType())) {
+            promise.fail("Failed to process data import event payload");
+          } else {
+            promise.complete(kafkaRecord.key());
+          }
+        });
       return promise.future();
     } catch (IOException e) {
       logger.error("Failed to process data import kafka record from topic {}", kafkaRecord.topic(), e);
       return Future.failedFuture(e);
     }
+  }
+
+  private Map<String, String> retrieveOkapiHeaders(DataImportEventPayload eventPayload) {
+    return Map.of(RestConstants.OKAPI_URL, eventPayload.getOkapiUrl(),
+      RestVerticle.OKAPI_HEADER_TENANT, eventPayload.getTenant(),
+      RestVerticle.OKAPI_HEADER_TOKEN, eventPayload.getToken());
   }
 }
