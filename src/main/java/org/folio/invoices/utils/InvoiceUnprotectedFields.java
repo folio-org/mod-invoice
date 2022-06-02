4ef99ee73521f89c019fa99d79359c991a541983
SinghAdes <90304030+SinghAdes@users.noreply.github.com>
2022-06-02 14:04:33 +0530

    [MODINVOICE-401]-Modified InvoiceValidator class

--- src/main/java/org/folio/invoices/utils/InvoiceUnprotectedFields.java
+++ src/main/java/org/folio/invoices/utils/InvoiceUnprotectedFields.java
@@ -5,8 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum InvoiceUnprotectedFields {
-
-  TAGS("tags");
+  PAYMENT_DUE("paymentDue"),
+  PAYMENT_METHOD("paymentMethod"),
+  VENDOR_INVOICE_NO("vendorInvoiceNo"),
+  ACQ_UNIT_IDs("acqUnitIds"),
+  NOTE("note"),
+  BILL_TO("billTo");
 
   InvoiceUnprotectedFields(String field) {
     this.field = field;
