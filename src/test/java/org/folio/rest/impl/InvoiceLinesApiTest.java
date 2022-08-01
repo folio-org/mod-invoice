7c53dee4f770752b699a62ae9b7047a4284f26a8
SinghAdes <90304030+SinghAdes@users.noreply.github.com>
2022-08-01 19:33:35 +0530

    [MODINVOICE-417]-Added tests

--- src/test/java/org/folio/rest/impl/InvoiceLinesApiTest.java
+++ src/test/java/org/folio/rest/impl/InvoiceLinesApiTest.java
@@ -75,6 +75,7 @@ public class InvoiceLinesApiTest extends ApiTestBase {
   static final String FUND_VALIDATOR_MOCK_DATA_PATH = BASE_MOCK_DATA_PATH + "fundsValidator/validateFundDistributionsRequest.json";
   static final String FUND_VALIDATOR_MOCK_DATA_PATH_DUP_ADJ_ID = BASE_MOCK_DATA_PATH + "fundsValidator/validateFundDistributionsRequest_duplicate_adjId.json";
   static final String FUND_VALIDATOR_MOCK_DATA_WITH_ZERO_PRICE = BASE_MOCK_DATA_PATH + "fundsValidator/validateFundDistributionsRequest_mixed_zero_price.json";
+  static final String FUND_VALIDATOR_MOCK_DATA_WITH_REMAINING_AMOUNT = BASE_MOCK_DATA_PATH + "fundsValidator/validateFundDistributionsRequest_with_remaining_amount.json";
   static final String INVOICE_LINES_LIST_PATH = INVOICE_LINES_MOCK_DATA_PATH + "invoice_lines.json";
   public static final String INVOICE_LINES_PATH = "/invoice/invoice-lines";
   public static final String INVOICE_LINE_ID_PATH = INVOICE_LINES_PATH + "/%s";
@@ -299,6 +300,18 @@ public class InvoiceLinesApiTest extends ApiTestBase {
   }
   @Test
   public void fundValidationWithZeroSubtotalTest()
+  {
+    ValidateFundDistributionsRequest reqData = getMockAsJson(FUND_VALIDATOR_MOCK_DATA_WITH_ZERO_PRICE).mapTo(ValidateFundDistributionsRequest.class);
+     verifyPut(INVOICE_LINE_FUNDS_VALIDATOR_ID_PATH, reqData, "", 422);
+  }
+  @Test
+  public void fundValidationWithRemainingAmountTest()
+  {
+    ValidateFundDistributionsRequest reqData = getMockAsJson(FUND_VALIDATOR_MOCK_DATA_WITH_REMAINING_AMOUNT).mapTo(ValidateFundDistributionsRequest.class);
+     verifyPut(INVOICE_LINE_FUNDS_VALIDATOR_ID_PATH, reqData, "", 422);
+  }
+  @Test
+  public void fundValidationWithRemainingTest()
   {
     ValidateFundDistributionsRequest reqData = getMockAsJson(FUND_VALIDATOR_MOCK_DATA_WITH_ZERO_PRICE).mapTo(ValidateFundDistributionsRequest.class);
      verifyPut(INVOICE_LINE_FUNDS_VALIDATOR_ID_PATH, reqData, "", 422);
