6585dc5aa6caba9fd917759c1c3568fbf8477558
Damien <damien-git@users.noreply.github.com>
2023-05-09 15:43:59 -0400

    [MODINVOICE-474] Removed unused import

--- src/main/java/org/folio/services/finance/transaction/EncumbranceService.java
+++ src/main/java/org/folio/services/finance/transaction/EncumbranceService.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.models.InvoiceWorkflowDataHolder;
