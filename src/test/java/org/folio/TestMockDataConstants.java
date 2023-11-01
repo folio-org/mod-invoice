54765220ca8674cb9e61acfb29b0b48e8be715a7
Khamidulla Abdulkhakimov <Khamidulla_Abdulkhakimov@epam.com>
2023-11-01 16:37:35 +0500

    [MODINVOICE-509] - Added tests

--- src/test/java/org/folio/TestMockDataConstants.java
+++ src/test/java/org/folio/TestMockDataConstants.java
@@ -4,9 +4,11 @@ public final class TestMockDataConstants {
   private TestMockDataConstants() {
   }
   public static final String BASE_MOCK_DATA_PATH = "mockdata/";
+  public static final String BASE_MOCK_BUDGETS_BASE_PATH = BASE_MOCK_DATA_PATH + "budgets/";
   public static final String BASE_MOCK_TRANSACTIONS_BASE_PATH = BASE_MOCK_DATA_PATH + "transactions/";
-  public static final String MOCK_TRANSACTIONS_LIST = BASE_MOCK_TRANSACTIONS_BASE_PATH + "transactions.json";
   public static final String MOCK_CREDITS_LIST = BASE_MOCK_TRANSACTIONS_BASE_PATH + "credits.json";
+  public static final String MOCK_BUDGET_ITEM = BASE_MOCK_BUDGETS_BASE_PATH + "budget.json";
+  public static final String MOCK_BUDGETS_LIST = BASE_MOCK_BUDGETS_BASE_PATH + "budgets.json";
   public static final String MOCK_ENCUMBRANCES_LIST = BASE_MOCK_TRANSACTIONS_BASE_PATH + "encumbrances.json";
   public static final String MOCK_PAYMENTS_LIST = BASE_MOCK_TRANSACTIONS_BASE_PATH + "payments.json";
   public static final String MOCK_PENDING_PAYMENTS_LIST = BASE_MOCK_TRANSACTIONS_BASE_PATH + "pending-payments.json";
