c54cb15b34e6f7a861f8138c3750b68eefc5c66f
Yuntian Hu <48864579+yuntianhu@users.noreply.github.com>
2023-12-06 06:46:39 -0500

    Update UserPermissionsUtil.java

--- src/main/java/org/folio/utils/UserPermissionsUtil.java
+++ src/main/java/org/folio/utils/UserPermissionsUtil.java
@@ -63,6 +63,42 @@ public final class UserPermissionsUtil {
     }
   }
 
+  public static void verifyPaidPermission(List<String> newAcqUnitIds, List<String> currentAcqUnitIds,
+                                                   Map<String, String> okapiHeaders) {
+    Set<String> newAcqUnits = new HashSet<>(CollectionUtils.emptyIfNull(newAcqUnitIds));
+    Set<String> acqUnitsFromStorage = new HashSet<>(CollectionUtils.emptyIfNull(currentAcqUnitIds));
+    // this varible is used to check if the inovice appovred 
+    Set<String> acqStatus = new HashSet<>(CollectionUtils.emptyIfNull(status));
+    // check if the invoice is approved. if the invoice had not been approved, the function will throw a expection. otherwise the function will check the if the user has permission. 
+    if (isApporved(acqStatus)){
+    throw new HttpException(HttpStatus.HTTP_FORBIDDEN.toInt(), USER_HAS_NO_ACQ_PERMISSIONS);
+      
+    }
+     else if (isManagePermissionRequired(newAcqUnits, acqUnitsFromStorage) && isUserDoesNotHaveDesiredPermission(MANAGE, okapiHeaders)) {
+      throw new HttpException(HttpStatus.HTTP_FORBIDDEN.toInt(), USER_HAS_NO_ACQ_PERMISSIONS);
+    }
+  }
+
+    /**
+   * The method checks if list of acquisition units to which the invoice is assigned is changed, if yes, then check that if the user
+   * has desired permission to approve acquisition units assignments
+   *
+   * @throws HttpException    if user does not have manage permission
+   * @param newAcqUnitIds     list of acquisition units coming from request
+   * @param currentAcqUnitIds list of acquisition units from storage
+   */
+
+  public static void verifyApprovalPermission(List<String> newAcqUnitIds, List<String> currentAcqUnitIds,
+                                                   Map<String, String> okapiHeaders) {
+    Set<String> newAcqUnits = new HashSet<>(CollectionUtils.emptyIfNull(newAcqUnitIds));
+    Set<String> acqUnitsFromStorage = new HashSet<>(CollectionUtils.emptyIfNull(currentAcqUnitIds));
+
+    if (isManagePermissionRequired(newAcqUnits, acqUnitsFromStorage) && isUserDoesNotHaveDesiredPermission(MANAGE, okapiHeaders)) {
+      throw new HttpException(HttpStatus.HTTP_FORBIDDEN.toInt(), USER_HAS_NO_ACQ_PERMISSIONS);
+    }
+  }
+
+
   /**
    * The method checks whether the user has the desired permission to update the fiscal year.
    *
@@ -82,6 +118,18 @@ public final class UserPermissionsUtil {
 
   private static boolean isManagePermissionRequired(Set<String> newAcqUnits, Set<String> acqUnitsFromStorage) {
     return !CollectionUtils.isEqualCollection(newAcqUnits, acqUnitsFromStorage);
+  }
+
+    /**
+   * The method checks whether the user has the desired permission to update the fiscal year.
+   *
+   * @throws HttpException if user does not have fiscal year update permission
+   * @param newFiscalYearId     fiscal year id coming from request
+   * @param fiscalYearIdFromStorage fiscal year id from storage
+   */
+
+  private static boolean isApporved(String newStatus) {
+    return !CollectionUtils.isEqualCollection(newStatus, "Approved");
   }
 
 }
