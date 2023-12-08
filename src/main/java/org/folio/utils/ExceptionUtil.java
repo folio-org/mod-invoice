c44dbf6234720d5e1a17e0c539de91034da933b4
Irakli Merabishvili <irakli_merabishvili1@epam.com>
2023-12-08 17:36:20 +0400

    null check added

--- src/main/java/org/folio/utils/ExceptionUtil.java
+++ src/main/java/org/folio/utils/ExceptionUtil.java
@@ -10,7 +10,8 @@ public final class ExceptionUtil {
 
   public static boolean matches(Throwable cause, ErrorCodes errorCode) {
     if (cause instanceof HttpException httpException) {
-      return httpException.getErrors().getErrors()
+      var errors = httpException.getErrors();
+      return errors != null && errors.getErrors() != null && errors.getErrors()
         .stream()
         .anyMatch(error -> error.getCode().equals(errorCode.getCode()));
     }
