670b0b8e4c31d43a462c9f2c610e9f34c5da7642
Yuntian Hu <48864579+yuntianhu@users.noreply.github.com>
2023-12-07 22:42:44 -0500

    Update UserPermissionsUtil.java

--- src/main/java/org/folio/utils/UserPermissionsUtil.java
+++ src/main/java/org/folio/utils/UserPermissionsUtil.java
@@ -19,18 +19,19 @@ import org.folio.invoices.rest.exceptions.HttpException;
 import org.folio.invoices.utils.AcqDesiredPermissions;
 
 import io.vertx.core.json.JsonArray;
+import org.folio.rest.jaxrs.model.Invoice;
 
 public final class UserPermissionsUtil {
   public static final String OKAPI_HEADER_PERMISSIONS = "X-Okapi-Permissions";
 
   private static final String EMPTY_ARRAY = "[]";
 
-  private UserPermissionsUtil(){
+  private UserPermissionsUtil() {
 
   }
 
   public static void verifyUserHasAssignPermission(List<String> acqUnitIds, Map<String, String> okapiHeaders) {
-    if (CollectionUtils.isNotEmpty(acqUnitIds) && isUserDoesNotHaveDesiredPermission(ASSIGN, okapiHeaders)){
+    if (CollectionUtils.isNotEmpty(acqUnitIds) && isUserDoesNotHaveDesiredPermission(ASSIGN, okapiHeaders)) {
       throw new HttpException(HttpStatus.HTTP_FORBIDDEN.toInt(), USER_HAS_NO_ACQ_PERMISSIONS);
     }
   }
@@ -49,9 +50,9 @@ public final class UserPermissionsUtil {
    * The method checks if list of acquisition units to which the invoice is assigned is changed, if yes, then check that if the user
    * has desired permission to manage acquisition units assignments
    *
-   * @throws HttpException if user does not have manage permission
    * @param newAcqUnitIds     list of acquisition units coming from request
    * @param currentAcqUnitIds list of acquisition units from storage
+   * @throws HttpException if user does not have manage permission
    */
   public static void verifyUserHasManagePermission(List<String> newAcqUnitIds, List<String> currentAcqUnitIds,
                                                    Map<String, String> okapiHeaders) {
@@ -63,12 +64,49 @@ public final class UserPermissionsUtil {
     }
   }
 
+  public static void verifyPaidPermission(List<String> newAcqUnitIds, List<String> currentAcqUnitIds, Invoice.Status invoiceStatus, Invoice.Status StatusFromStorage,
+                                          Map<String, String> okapiHeaders) {
+    Set<String> newAcqUnits = new HashSet<>(CollectionUtils.emptyIfNull(newAcqUnitIds));
+    Set<String> acqUnitsFromStorage = new HashSet<>(CollectionUtils.emptyIfNull(currentAcqUnitIds));
+
+    // this varible is used to check if the inovice appovred
+    //Set<String> acqStatus = new HashSet<>(CollectionUtils.emptyIfNull(status));
+    // check if the invoice is approved. if the invoice had not been approved, the function will throw a expection. otherwise the function will check the if the user has permission.
+
+    if (WasThisInvoiceApporved(invoiceStatus) && WasThisInvoicePaid (invoiceStatus) && New_Invoice_Status_Request (invoiceStatus,StatusFromStorage)){
+      throw new HttpException(HttpStatus.HTTP_FORBIDDEN.toInt(), USER_HAS_NO_ACQ_PERMISSIONS);
+
+    } else if (isManagePermissionRequired(newAcqUnits, acqUnitsFromStorage) && isUserDoesNotHaveDesiredPermission(MANAGE, okapiHeaders)) {
+      throw new HttpException(HttpStatus.HTTP_FORBIDDEN.toInt(), USER_HAS_NO_ACQ_PERMISSIONS);
+    }
+  }
+
+  /**
+   * The method checks if list of acquisition units to which the invoice is assigned is changed, if yes, then check that if the user
+   * has desired permission to approve acquisition units assignments
+   *
+   * @param newAcqUnitIds     list of acquisition units coming from request
+   * @param currentAcqUnitIds list of acquisition units from storage
+   * @throws HttpException if user does not have manage permission
+   */
+
+  public static void verifyApprovalPermission(List<String> newAcqUnitIds, List<String> currentAcqUnitIds,
+                                              Map<String, String> okapiHeaders) {
+    Set<String> newAcqUnits = new HashSet<>(CollectionUtils.emptyIfNull(newAcqUnitIds));
+    Set<String> acqUnitsFromStorage = new HashSet<>(CollectionUtils.emptyIfNull(currentAcqUnitIds));
+
+    if (isManagePermissionRequired(newAcqUnits, acqUnitsFromStorage) && isUserDoesNotHaveDesiredPermission(MANAGE, okapiHeaders)) {
+      throw new HttpException(HttpStatus.HTTP_FORBIDDEN.toInt(), USER_HAS_NO_ACQ_PERMISSIONS);
+    }
+  }
+
+
   /**
    * The method checks whether the user has the desired permission to update the fiscal year.
    *
-   * @throws HttpException if user does not have fiscal year update permission
-   * @param newFiscalYearId     fiscal year id coming from request
+   * @param newFiscalYearId         fiscal year id coming from request
    * @param fiscalYearIdFromStorage fiscal year id from storage
+   * @throws HttpException if user does not have fiscal year update permission
    */
   public static void verifyUserHasFiscalYearUpdatePermission(String newFiscalYearId, String fiscalYearIdFromStorage, Map<String, String> okapiHeaders) {
     if (isFiscalYearUpdated(newFiscalYearId, fiscalYearIdFromStorage) && isUserDoesNotHaveDesiredPermission(FISCAL_YEAR_UPDATE, okapiHeaders)) {
@@ -84,4 +122,31 @@ public final class UserPermissionsUtil {
     return !CollectionUtils.isEqualCollection(newAcqUnits, acqUnitsFromStorage);
   }
 
+  /**
+   * The method checks whether the user has the desired permission to update the fiscal year.
+   *
+   * @param newFiscalYearId         fiscal year id coming from request
+   * @param fiscalYearIdFromStorage fiscal year id from storage
+   * @throws HttpException if user does not have fiscal year update permission
+   */
+
+  private static boolean WasThisInvoiceApporved (Invoice.Status newStatus ) {
+    //  return !CollectionUtils.isEqualCollection(newStatus, "Approved");
+
+    return !newStatus.value().equals("Approved");
+
+  }
+
+  private static boolean WasThisInvoicePaid (Invoice.Status newStatus ) {
+    //  return !CollectionUtils.isEqualCollection(newStatus, "Approved");
+
+    return !newStatus.value().equals("Paid");
+
+  }
+
+  private static boolean New_Invoice_Status_Request(Invoice.Status newStatus, Invoice.Status StatusFromStorage ) {
+
+    return !newStatus.value().equals(StatusFromStorage.value());
+
+  }
 }
