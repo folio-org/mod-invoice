d5d12ed46c259b1b405e3e28026782f825a1f802
Yuntian Hu <48864579+yuntianhu@users.noreply.github.com>
2023-12-13 21:41:25 -0500

    Update UserPermissionsUtil.java

--- src/main/java/org/folio/utils/UserPermissionsUtil.java
+++ src/main/java/org/folio/utils/UserPermissionsUtil.java
@@ -67,8 +67,8 @@ public final class UserPermissionsUtil {
     }
   }
 
-  public static void  verifyUserHasInvoicePayPermission  (Invoice.Status newInvoiceStatus, Invoice.Status statusFromStorage,   Map<String, String> okapiHeaders ){
-    if (isInvoiceStatusUpdated(newInvoiceStatus, statusFromStorage)) && isUserDoesNotHaveDesiredPermission(PAY, okapiHeaders)){
+  public static void  verifyUserHasInvoicePayPermission  (Invoice.Status newInvoiceStatus, Invoice.Status statusFromStorage, Map<String, String> okapiHeaders ){
+    if (isInvoiceStatusUpdated(newInvoiceStatus, statusFromStorage) && isUserDoesNotHaveDesiredPermission(PAY, okapiHeaders)){
       throw new HttpException(HttpStatus.HTTP_FORBIDDEN.toInt(),  USER_HAS_NO_PERMISSIONS);
     }
  }
