feaefc24086bffdcacf895302dc71c9ad05f6808
JavokhirAbdullayev <Javokhir_Abdullaev@epam.com>
2024-01-22 16:01:00 +0500

    add timestamp column in records_invoices table

--- src/main/java/org/folio/domain/relationship/EntityTable.java
+++ src/main/java/org/folio/domain/relationship/EntityTable.java
@@ -1,27 +1,14 @@
 package org.folio.domain.relationship;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum EntityTable {
   INVOICES("records_invoices", "record_id", "invoice_id");
 
   private final String tableName;
   private final String recordIdFieldName;
   private final String entityIdFieldName;
-
-  EntityTable(String tableName, String recordIdFieldName, String entityIdFieldName) {
-    this.tableName = tableName;
-    this.recordIdFieldName = recordIdFieldName;
-    this.entityIdFieldName = entityIdFieldName;
-  }
-
-  public String getTableName() {
-    return tableName;
-  }
-
-  public String getRecordIdFieldName() {
-    return recordIdFieldName;
-  }
-
-  public String getEntityIdFieldName() {
-    return entityIdFieldName;
-  }
 }
--- src/main/resources/templates/db_scripts/create_records_invoices_table.sql
+++ src/main/resources/templates/db_scripts/create_records_invoices_table.sql
@@ -1,5 +1,6 @@
 -- Creates table to store records and invoice lines
 CREATE TABLE IF NOT EXISTS records_invoices (
   record_id uuid NOT NULL PRIMARY KEY,
-  invoice_id uuid NOT NULL
+  invoice_id uuid NOT NULL,
+  created_date timestamp DEFAULT CURRENT_TIMESTAMP
 );
