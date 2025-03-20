b9f72871105637a69fb366433f476ced270caf62
Boburbek Kadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-03-20 11:15:47 +0500

    [MODINVOICE-576]. Add new implementation to VAT included-in adjustment to both Invoices and Invoice Lines (#529)
    * [MODINVOICE-576]. Add new implementation to VAT included-in adjustment to both Invoices and Invoice Lines
    
    * [MODINVOICE-576]. Add new Included In calculation at the Invoice Line level
    
    * [MODINVOICE-576]. Update comments
    
    * [MODINVOICE-576]. Apply review recommendations
    
    * [MODINVOICE-576]. Rename a method
    
    * [MODINVOICE-576]. Remove shallow copy & simplify code

--- src/test/java/org/folio/rest/impl/InvoiceLinesProratedAdjustmentsTest.java
+++ src/test/java/org/folio/rest/impl/InvoiceLinesProratedAdjustmentsTest.java
@@ -11,6 +11,9 @@ import static org.folio.rest.impl.MockServer.addMockEntry;
 import static org.folio.rest.impl.MockServer.getInvoiceLineCreations;
 import static org.folio.rest.impl.MockServer.getInvoiceLineUpdates;
 import static org.folio.rest.impl.MockServer.getInvoiceUpdates;
+import static org.folio.rest.jaxrs.model.Adjustment.Prorate.BY_AMOUNT;
+import static org.folio.rest.jaxrs.model.Adjustment.RelationToTotal.INCLUDED_IN;
+import static org.folio.rest.jaxrs.model.Adjustment.Type.PERCENTAGE;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -19,6 +22,7 @@ import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 import io.vertx.junit5.VertxExtension;
 import java.util.Collections;
+import java.util.UUID;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,6 +31,7 @@ import org.folio.rest.jaxrs.model.Invoice;
 import org.folio.rest.jaxrs.model.InvoiceLine;
 import org.folio.rest.jaxrs.model.InvoiceLineCollection;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -72,13 +77,12 @@ public class InvoiceLinesProratedAdjustmentsTest extends ApiTestBase {
      * "Percentage" - this is the percentage of subTotal i.e. 15% of 25$ = 3.75$
      */
     double expectedAdjTotal = (type == Adjustment.Type.AMOUNT) ? 15d : 3.75d;
-    double expectedAdjValue = expectedAdjTotal;
     assertThat(invoiceLine.getAdjustments(), hasSize(1));
     assertThat(invoiceLine.getAdjustmentsTotal(), is(expectedAdjTotal));
 
-    Adjustment lineAdjustment = invoiceLine.getAdjustments().get(0);
+    Adjustment lineAdjustment = invoiceLine.getAdjustments().getFirst();
     verifyInvoiceLineAdjustmentCommon(invoiceAdjustment, lineAdjustment);
-    assertThat(lineAdjustment.getValue(), is(expectedAdjValue));
+    assertThat(lineAdjustment.getValue(), is(expectedAdjTotal));
   }
 
   @ParameterizedTest
@@ -123,13 +127,12 @@ public class InvoiceLinesProratedAdjustmentsTest extends ApiTestBase {
      * "Percentage" - this is the percentage of subTotal i.e. 15% of 25$ = 3.75$
      */
     double expectedAdjTotal = (type == Adjustment.Type.AMOUNT) ? 15d : 3.75d;
-    double expectedAdjValue = expectedAdjTotal;
     assertThat(lineToStorage.getAdjustments(), hasSize(1));
     assertThat(lineToStorage.getAdjustmentsTotal(), is(expectedAdjTotal));
 
-    Adjustment lineAdjustment = lineToStorage.getAdjustments().get(0);
+    Adjustment lineAdjustment = lineToStorage.getAdjustments().getFirst();
     verifyInvoiceLineAdjustmentCommon(invoiceAdjustment, lineAdjustment);
-    assertThat(lineAdjustment.getValue(), is(expectedAdjValue));
+    assertThat(lineAdjustment.getValue(), is(expectedAdjTotal));
   }
 
   @ParameterizedTest
@@ -172,14 +175,89 @@ public class InvoiceLinesProratedAdjustmentsTest extends ApiTestBase {
      * "Percentage" - this is the percentage of subTotal i.e. 15% of 25$ = 3.75$
      */
     double expectedAdjTotal = (type == Adjustment.Type.AMOUNT) ? 15d : 3.75d;
-    double expectedAdjValue = expectedAdjTotal;
 
     assertThat(lineToStorage.getAdjustments(), hasSize(1));
     assertThat(lineToStorage.getAdjustmentsTotal(), is(expectedAdjTotal));
 
-    Adjustment lineAdjustment = lineToStorage.getAdjustments().get(0);
+    Adjustment lineAdjustment = lineToStorage.getAdjustments().getFirst();
     verifyInvoiceLineAdjustmentCommon(invoiceAdjustment, lineAdjustment);
-    assertThat(lineAdjustment.getValue(), is(expectedAdjValue));
+    assertThat(lineAdjustment.getValue(), is(expectedAdjTotal));
+  }
+
+  @Test
+  public void testCreateInvoiceWithOnePercentageTypeByAmountProrateIncludedByTotalAdjustment() {
+    logger.info("=== Creating invoice with one adjustment by amount prorate included by total ===");
+
+    // Prepare data "from storage"
+    Invoice invoice = getMockAsJson(OPEN_INVOICE_SAMPLE_PATH).mapTo(Invoice.class).withId(randomUUID().toString());
+    Adjustment invoiceAdjustment = new Adjustment()
+      .withId(UUID.randomUUID().toString())
+      .withDescription("VAT")
+      .withProrate(BY_AMOUNT)
+      .withType(PERCENTAGE)
+      .withRelationToTotal(INCLUDED_IN)
+      .withValue(7d);
+    invoice.withAdjustments(Collections.singletonList(invoiceAdjustment));
+    addMockEntry(INVOICES, invoice);
+
+    // Prepare request body
+    InvoiceLine invoiceLineBody = getMockInvoiceLine(invoice.getId()).withAdjustmentsTotal(0d).withSubTotal(30d).withQuantity(1);
+
+    // Send create request
+    InvoiceLine invoiceLine = verifySuccessPost(INVOICE_LINES_PATH, invoiceLineBody).as(InvoiceLine.class);
+
+    // Verification
+    assertThat(getInvoiceLineUpdates(), Matchers.hasSize(0));
+    assertThat(getInvoiceUpdates(), Matchers.hasSize(1));
+    compareRecordWithSentToStorage(invoiceLine);
+
+    assertThat(invoiceLine.getAdjustments(), hasSize(1));
+    assertThat(invoiceLine.getAdjustmentsTotal(), is(1.96d));
+    assertThat(invoiceLine.getSubTotal(), is(28.04d));
+
+    Adjustment lineAdjustment = invoiceLine.getAdjustments().getFirst();
+    verifyInvoiceLineAdjustmentCommon(invoiceAdjustment, lineAdjustment);
+    assertThat(lineAdjustment.getValue(), is(1.96d));
+  }
+
+  @Test
+  public void testDeleteInvoiceWithOnePercentageTypeByAmountProrateIncludedByTotalAdjustment() {
+    logger.info("=== Deleting invoice with one adjustment by amount prorate included by total ===");
+
+    // Prepare data "from storage"
+    Invoice invoice = getMockAsJson(OPEN_INVOICE_SAMPLE_PATH).mapTo(Invoice.class).withId(randomUUID().toString());
+    Adjustment invoiceAdjustment = new Adjustment()
+      .withId(UUID.randomUUID().toString())
+      .withDescription("VAT")
+      .withProrate(BY_AMOUNT)
+      .withType(PERCENTAGE)
+      .withRelationToTotal(INCLUDED_IN)
+      .withValue(7d);
+    invoice.withAdjustments(Collections.singletonList(invoiceAdjustment));
+    addMockEntry(INVOICES, invoice);
+
+    InvoiceLine line1 = getMockInvoiceLine(invoice.getId()).withAdjustmentsTotal(0d).withSubTotal(30d).withQuantity(1);
+    addMockEntry(INVOICE_LINES, line1);
+    InvoiceLine line2 = getMockInvoiceLine(invoice.getId()).withAdjustmentsTotal(0d).withSubTotal(30d).withQuantity(1);
+    addMockEntry(INVOICE_LINES, line2);
+
+    // Send delete request
+    verifyDeleteResponse(String.format(INVOICE_LINE_ID_PATH, line2.getId()), "", 204);
+
+    // Verification
+    assertThat(getInvoiceLineUpdates(), Matchers.hasSize(1));
+    assertThat(getInvoiceUpdates(), Matchers.hasSize(1));
+
+    InvoiceLine lineToStorage = getLineToStorageById(line1.getId());
+    assertThat(lineToStorage.getAdjustments(), hasSize(1));
+
+    assertThat(lineToStorage.getAdjustments(), hasSize(1));
+    assertThat(lineToStorage.getAdjustmentsTotal(), is(1.96d));
+    assertThat(lineToStorage.getSubTotal(), is(28.04));
+
+    Adjustment lineAdjustment = lineToStorage.getAdjustments().getFirst();
+    verifyInvoiceLineAdjustmentCommon(invoiceAdjustment, lineAdjustment);
+    assertThat(lineAdjustment.getValue(), is(1.96d));
   }
 
   private InvoiceLine getLineToStorageById(String invoiceLineId) {
@@ -187,13 +265,13 @@ public class InvoiceLinesProratedAdjustmentsTest extends ApiTestBase {
       .filter(line -> invoiceLineId.equals(line.getString("id")))
       .findAny()
       .map(obj -> obj.mapTo(InvoiceLine.class))
-      .get();
+      .orElseThrow();
   }
 
   private InvoiceLine getMockInvoiceLine(String invoiceId) {
     InvoiceLine invoiceLine = getMockAsJson(INVOICE_LINES_LIST_PATH).mapTo(InvoiceLineCollection.class)
       .getInvoiceLines()
-      .get(0)
+      .getFirst()
       .withId(randomUUID().toString())
       .withInvoiceId(invoiceId);
     invoiceLine.getAdjustments().clear();
@@ -214,7 +292,7 @@ public class InvoiceLinesProratedAdjustmentsTest extends ApiTestBase {
   private void compareRecordWithSentToStorage(InvoiceLine invoiceLine) {
     // Verify that invoice line sent to storage is the same as in response
     assertThat(getInvoiceLineCreations(), Matchers.hasSize(1));
-    InvoiceLine invoiceLineToStorage = getInvoiceLineCreations().get(0).mapTo(InvoiceLine.class);
+    InvoiceLine invoiceLineToStorage = getInvoiceLineCreations().getFirst().mapTo(InvoiceLine.class);
     assertThat(invoiceLine, equalTo(invoiceLineToStorage));
   }
 }
