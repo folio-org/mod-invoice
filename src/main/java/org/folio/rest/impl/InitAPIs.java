ef9e2a018cc4a976890277dafb5e52e16b4e9010
aliaksandr_fedasiuk <aliaksandr_fedasiuk@epam.com>
2025-09-19 20:54:50 +0500

    MODINVOICE-526: start new CancelledJobConsumerVerticle by InitAPIs

--- src/main/java/org/folio/rest/impl/InitAPIs.java
+++ src/main/java/org/folio/rest/impl/InitAPIs.java
@@ -1,28 +1,29 @@
 package org.folio.rest.impl;
 
-import javax.money.convert.MonetaryConversions;
-
+import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.ThreadingModel;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.jackson.DatabindCodec;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.config.ApplicationConfig;
 import org.folio.dbschema.ObjectMapperTool;
 import org.folio.rest.resource.interfaces.InitAPI;
 import org.folio.spring.SpringContextUtil;
+import org.folio.verticles.CancelledJobConsumerVerticle;
 import org.folio.verticles.DataImportConsumerVerticle;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.support.AbstractApplicationContext;
 
-import com.fasterxml.jackson.databind.DeserializationConfig;
-import com.fasterxml.jackson.databind.SerializationConfig;
-
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Context;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.core.Promise;
-import io.vertx.core.Vertx;
-import io.vertx.core.json.jackson.DatabindCodec;
+import javax.money.convert.MonetaryConversions;
+import java.util.List;
 
 /**
  * The class initializes vertx context adding spring context
@@ -33,6 +34,9 @@ public class InitAPIs implements InitAPI {
 
   @Value("${mod.invoice.kafka.DataImportConsumerVerticle.instancesNumber:1}")
   private int dataImportConsumerVerticleNumber;
+
+  @Value("${mod.invoice.kafka.CancelledJobConsumerVerticle.instancesNumber:1}")
+  private int cancelledJobConsumerVerticleNumber;
 
   @Value("${dataimport.consumer.verticle.mandatory:false}")
   private boolean isConsumerVerticleMandatory;
@@ -71,18 +75,42 @@ public class InitAPIs implements InitAPI {
     });
   }
 
-  private Future<String> deployDataImportConsumerVerticle(Vertx vertx) {
-    Promise<String> promise = Promise.promise();
+  private Future<Void> deployDataImportConsumerVerticle(Vertx vertx) {
     AbstractApplicationContext springContext = vertx.getOrCreateContext().get("springContext");
 
-    DeploymentOptions deploymentOptions = new DeploymentOptions()
-      .setInstances(dataImportConsumerVerticleNumber)
-      .setWorker(true);
-    vertx.deployVerticle(() -> springContext.getBean(DataImportConsumerVerticle.class), deploymentOptions, promise);
+    List<Future<?>> deploymentFutures = List.of(
+      deployVerticle(vertx, springContext, DataImportConsumerVerticle.class, dataImportConsumerVerticleNumber, ThreadingModel.WORKER),
+      deployVerticle(vertx, springContext, CancelledJobConsumerVerticle.class, cancelledJobConsumerVerticleNumber, ThreadingModel.EVENT_LOOP)
+    );
 
-    return promise.future()
-      .onSuccess(ar -> log.info("DataImportConsumerVerticle verticle was successfully started"))
-      .onFailure(e -> log.error("DataImportConsumerVerticle verticle was not successfully started", e));
+    return Future.all(deploymentFutures).onComplete(ar -> {
+        if (ar.succeeded()) {
+          log.info("All consumer verticles deployed successfully.");
+        } else {
+          log.error("Failed to deploy one or more consumer verticles", ar.cause());
+        }
+      })
+      .map((Void) null);
+  }
+
+  /**
+   * Deploys a verticle from the Spring context with the given options.
+   *
+   * @param vertx          The Vert.x instance.
+   * @param springContext  The Spring application context.
+   * @param verticleClass  The class of the verticle to deploy.
+   * @param instances      The number of instances to deploy.
+   * @param threadingModel The ThreadingModel value.
+   * @return A Future that completes with the deployment ID.
+   */
+  private Future<String> deployVerticle(Vertx vertx, AbstractApplicationContext springContext,
+                                        Class<? extends AbstractVerticle> verticleClass,
+                                        int instances, ThreadingModel threadingModel) {
+    DeploymentOptions options = new DeploymentOptions()
+      .setInstances(instances)
+      .setThreadingModel(threadingModel);
+
+    return vertx.deployVerticle(() -> springContext.getBean(verticleClass), options);
   }
 
   private void initJavaMoney() {
