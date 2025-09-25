4e7fffbe141bfcb5ba96b8bbdcd011f6c39bed39
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2025-09-25 20:44:07 +0500

    [MODINVOICE-613]. Apply review recommendations

--- src/main/java/org/folio/services/finance/EncumbranceUtils.java
+++ src/main/java/org/folio/services/finance/EncumbranceUtils.java
@@ -1,13 +1,21 @@
 package org.folio.services.finance;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.log4j.Log4j2;
 import org.folio.rest.acq.model.finance.Encumbrance;
 import org.folio.rest.acq.model.finance.Transaction;
 
+import java.util.Objects;
+
+@Log4j2
 @UtilityClass
 public class EncumbranceUtils {
 
   public static boolean allowEncumbranceToUnrelease(Transaction encumbranceTransaction) {
+    if (Objects.isNull(encumbranceTransaction) || Objects.isNull(encumbranceTransaction.getEncumbrance())) {
+      log.warn("allowEncumbranceToUnrelease:: Invalid transaction or encumbrance");
+      return false;
+    }
     var encumbrance = encumbranceTransaction.getEncumbrance();
     return encumbrance.getStatus() == Encumbrance.Status.RELEASED &&
       (encumbrance.getAmountExpended() == 0
